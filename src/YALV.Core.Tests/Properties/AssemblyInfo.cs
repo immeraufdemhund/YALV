b6606b6f8a9e2808e068d713824477b924b99064
Robert Snyder <immeraufdemhund@hotmail.com>
2017-02-15 12:28:23 -0500

    Added empty nunit project

---
+++ src/YALV.Core.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("YALV.Core.Tests")]
+[assembly: AssemblyCompany("Luca Petrini")]
+[assembly: AssemblyProduct("YALV.Core.Tests")]
+[assembly: AssemblyCopyright("Copyright © Luca Petrini 2013")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
